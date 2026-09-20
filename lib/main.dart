08f6a5d2dd1dabcdcf895f04ebd4709d4a8a7627
rendle-swfc <ryan.endlein@gmail.com>
2026-09-20 08:55:30 -0400

    Initial commit with Flutter frontend and Flask backend

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'card_builder_screen.dart';
+
+void main() {
+  runApp(const CardGeneratorApp());
+}
+
+class CardGeneratorApp extends StatelessWidget {
+  const CardGeneratorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SWFC Card Generator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const CardBuilderScreen(),
+    );
+  }
+}
